edaefae1d7f53eb0f164882943f590ebf3aeb5a8
herdal06 <h.erdal721@gmail.com>
2022-08-22 01:05:21 +0300

    Create fragment and viewmodel for episodes

---
+++ app/src/main/java/com/herdal/paging3/presentation/episodes/EpisodesViewModel.kt
@@ -0,0 +1,9 @@
+package com.herdal.paging3.presentation.episodes
+
+import androidx.lifecycle.ViewModel
+import com.herdal.paging3.domain.repository.CharacterRepository
+import javax.inject.Inject
+
+class EpisodesViewModel @Inject constructor() :
+    ViewModel() {
+}
